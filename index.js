7136f6c6bb019b2fe143f6bbbdcca584f0367bc2
Danny Gleason <danny.c.gleason@gmail.com>
2022-04-28 16:27:25 -0400

    changed cors to *

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 3001;
 const bundle_model = require('./db_functions.js');
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
