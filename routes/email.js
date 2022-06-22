315b0c66835c98921aba643e48d45083f135d6ad
Danny Gleason <danny.c.gleason@gmail.com>
2022-06-22 13:27:58 -0400

    edited gmail code

--- routes/email.js
+++ routes/email.js
@@ -5,17 +5,18 @@ const router = express.Router()
 //email send to gift contributors 
 
 router.post('/send', (req, res) => {
-    res.send('email');
+   console.log('email inside of post request');
+   console.log('gmail client id' + process.env.GMAIL_CLIENT_ID)
   
     var name = req.body.recipient
     var email = req.body.email
-    var giftCode = req.body.giftCode
+    //var giftCode = req.body.giftCode
     var ownerName = req.body.ownerName
     var giftOwnerMessage = req.body.giftOwnerMessage;
   
     console.log('emails' + email);
     console.log('questions' + question);
-    console.log('inside post request' + unique_id);
+
       const OAuth2 = google.auth.OAuth2
     
       const OAuth2_client = new OAuth2(process.env.GMAIL_CLIENT_ID, process.env.GMAIL_CLIENT_SECRET);
@@ -37,7 +38,7 @@ router.post('/send', (req, res) => {
     })
       
       const mail_options_two = {
-        from: 'Amore Books <dan@amorebooks.io',
+        from: 'Amore Books <dan@amorebooks.io', 
         to: email, 
         subject: ownerName + 'selected you to contribute in a gift for ' + recipient + ' !',
         html: '<p> You have been selected to contribute to a Amore Books gift book! This means that' + ownerName + 'has asked you to write a positive or loving message for ' + name + '.  Your gift code is ' + giftCode  + '.  This message is from ' + ownerName+ ': ' + giftOwnerMessage + '. To contribute, go to ' + '<a href="https://amorebooks.io/write">www.amorebooks.io/write</a></p>'
