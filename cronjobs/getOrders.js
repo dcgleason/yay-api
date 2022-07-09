11939d8cb28151608aca716f0b2e3df703e34370
Danny Gleason <danny.c.gleason@gmail.com>
2022-07-09 16:12:43 -0400

    made date compatable with mongodb timestamp

--- cronjobs/getOrders.js
+++ cronjobs/getOrders.js
@@ -14,19 +14,21 @@ let dotenv = require('dotenv');
 dotenv.config()
 
 
+var readyToSend = []
+var associatedMessages = []
 
 const mongoOrderCollect = async () => {
 
     var fiveDays = 432000 * 1000 
-    var fiveDaysAgo = new Date(Date.now() - fiveDays).getTime(); //5 days ago in milliseconds
+    var fiveDaysAgo = new Date(Date.now() - fiveDays) //5 days ago in ISODate - which is the format of the MongoDB timestamp
   
     try {
-     const client = new MongoClient(url);
+    const client = new MongoClient(url);
     await client.connect();
     
     const gifts = client.db("yay_gift_orders").collection("gift_orders");
     const update = await gifts.updateMany(
-      { 'createdAt': { $lte: fiveDaysAgo }}, // if the createdAt date is less than or equal to 5 days ago...
+      { 'createdAt': { $lte: ISODate(fiveDaysAgo) }}, // if the createdAt date is less than or equal to 5 days ago...
       { $set: { "gift.fiveDays": true}} // set fiveDays field to true, to mark that it's been five days 
     );
     console.log(update);
@@ -35,7 +37,7 @@ const mongoOrderCollect = async () => {
     )
     
     results.forEach((gift) => {
-      todaysOrders.push(gift);
+      readyToSend.push(gift);
       })
   
     return true;
@@ -55,13 +57,13 @@ const mongoOrderCollect = async () => {
     
         for(var i =0; i<todaysOrders.length; i++ ){ //loop through orders and match all giftCodes to message objects
         const results = messages.find(
-          {"giftCode": todaysOrders[i].gift.giftCode}
+          {"giftCode": readyToSend[i].gift.giftCode}
         );
     
         const gifts = client.db("yay_gift_orders").collection("gift_orders");
         
         results.forEach((message) => { //for each message object find the gift order and push the message object with a mathing giftCode into the message array inside the associated gift Order
-          todaysMessages.push(message);
+          associatedMessages.push(message);
           gifts.findOneAndUpdate(
             {'gift.giftCode': message.giftCode},
             { $push: { 'messages': message}} // the message object with name, message (arr), and gift code are all push into the message array in the orders document
