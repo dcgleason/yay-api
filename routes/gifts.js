c0f701bcb0b65edba613db5fe4c679d0a58386e4
Danny Gleason <danny.c.gleason@gmail.com>
2022-07-09 12:20:10 -0400

    added insert order route

--- routes/gifts.js
+++ routes/gifts.js
@@ -33,4 +33,54 @@ router.get('/about', (req,res)=>{
     res.send("About GIFTS page")
 })
 
+router.post('/insertOrder', async (req, res) => {
+    console.log('createOrder');
+  
+    var createdAt= req.body.createdAt
+    var ownerName = req.body.owner.ownerName;
+    var ownerEmail = req.body.owner.ownerEmail;
+    var address = req.body.owner.shipping.address;
+    var city = req.body.owner.shipping.city;
+    var state = req.body.owner.shipping.state;
+    var zipCode = req.body.owner.shipping.zipCode;
+    var country = req.body.owner.shipping.country;
+    var phone = req.body.owner.shipping.phone;
+    var giftCode = req.body.gift.giftCode
+    var name = req.body.gift.recipient
+  
+    var data = JSON.stringify({
+            "createdAt": createdAt,
+            "owner": {
+              "ownerName": ownerName,
+              "ownerEmail": ownerEmail,
+              "shipping": {
+                "address": address,
+                "city": city,
+                "state": state,
+                "zip": zipCode,
+                "country": country,
+                "phone": phone,
+              }
+            },
+            "gift": {
+                "giftCode": giftCode,
+                "recipient": name,
+                "messages": [],
+                "twoWeeks": false,
+                "sent": false
+            }
+    });
+    try {
+      const client = new MongoClient(url);
+      await client.connect();
+     
+     const gifts = client.db("yay_gift_orders").collection("gift_orders");
+     const results = gifts.insertOne(data);
+     console.log(`An order document was inserted with the _id: ${results.insertedId}`);
+     } 
+     finally {
+     await client.close();
+     }
+  });
+
 module.exports = router
