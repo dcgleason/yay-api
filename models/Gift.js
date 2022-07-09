0f1b553a31d1df35954d72854e9c140b359e4067
Danny Gleason <danny.c.gleason@gmail.com>
2022-07-09 12:39:40 -0400

    added to gift schema

--- models/Gift.js
+++ models/Gift.js
@@ -1,22 +1,41 @@
 
-        const mongoose = require('mongoose')
+        const mongoose = require('mongoose');
+const { string } = require('pg-format');
         const Schema = mongoose.Schema
         /**
          * each gift contains a title 
          * the ID of the initiating user
          * and the array of contributorIDs - each page will belong to one contributor (dg - what do you mean by this?)
          */
-        const giftSchema = new Schema({
-            title: String,
-            //gifts have one user
-            userID: String,
-            //contributers are an array of contributer ids
-            contributorIDs:[String],
-            
+        const addressSchema = new Schema({
+          address: String,
+          city: String,
+          state: String,
+          zip: String,
+          phone: String
+        })
 
-          },
-          { timestamps: true }
-          );
+        const ownerSchema = new Schema({
+          ownerName: String,
+          ownerEmail: String,
+          shipping: addressSchema
+        })
+
+        const giftSchema = new Schema({
+          owner: ownerSchema,
+          //gifts have one owner
+          uniqueId: String,
+           // uniqueId is unqiue to the user
+          messages: [String],
+          //contributers are an array of contributer ids
+          contributorIDs:[String],
+          twoWeeks: false,
+          sent: false
+          
+
+        },
+        { timestamps: true }
+        );
 
 // inputting data to mongoDB database
 
