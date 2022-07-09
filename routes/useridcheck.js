89d4eca35dfb3ebc6a1b261238e9747c6e777b50
Danny Gleason <danny.c.gleason@gmail.com>
2022-07-09 13:33:54 -0400

    added to unique id check

--- routes/useridcheck.js
+++ routes/useridcheck.js
@@ -10,7 +10,7 @@ const url = 'mongodb+srv://dcgleason:F1e2n3n4!!@yay-cluster01.lijs4.mongodb.net/
 //gifts Home page
 router.post('/check', async (req, res) => { 
 
-    var giftCode = req.body.gifCode
+    var giftCode = req.body.giftCode
   
     try {
       const client = new MongoClient(url);
@@ -18,7 +18,7 @@ router.post('/check', async (req, res) => {
       console.log('unique - after mongo connect')
      
      const gifts = client.db("yay_gift_orders").collection("gift_orders");
-     const order = gifts.findOne({"giftCode": giftCode});
+     const order = gifts.findOne({"gift.giftCode": giftCode});
      console.log('order results' + order);
      if(order){
        console.log('in if statement')
