27d34a40ec5f85651300073b6f6383b2cae8e909
Danny Gleason <danny.c.gleason@gmail.com>
2022-07-10 13:07:13 -0400

    fixed error

--- cronjobs/reminderEmails.js
+++ cronjobs/reminderEmails.js
@@ -7,7 +7,7 @@ cron.schedule('* * 12 * * 0-6', () => {
     console.log('Checking for emails to send every day 12:00 pm at America/New_York timezone. Searche for orders that were created 2 days ago - first email reminder');
     // email code
     }
-}, {
+    , {
     scheduled: false,
     timezone: "America/New_York"
 });
@@ -17,7 +17,7 @@ cron.schedule('* * 12 * * 0-6', () => {
     console.log('Checking for emails to send every day 12:00 pm at America/New_York timezone. Searche for orders that were created 3 days ago - second email reminder');
     // email code
     }
-}, {
+    , {
     scheduled: false,
     timezone: "America/New_York"
 });
@@ -26,7 +26,7 @@ cron.schedule('* * 12 * * 0-6', () => {
     console.log('Checking for emails to send every day 12:00 pm at America/New_York timezone. Searche for orders that were created 4 days ago - final email reminder');
     // email code
     }
-}, {
+    , {
     scheduled: false,
     timezone: "America/New_York"
 });
