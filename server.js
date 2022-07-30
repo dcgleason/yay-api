e2bd45cc04fd640663fae49410449025806238a3
Dan Gleason <dannycgleason@gmail.com>
2022-07-30 16:31:58 -0400

    changed cors to allow all

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const stripe = require('stripe')('sk_test_51KtCf1LVDYVdzLHCA31MSSlOKhe7VQtXqJJiP
 
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "");
-    const allowedOrigins = ['http://localhost:3000', 'https://bundle.love'];
+    // const allowedOrigins = ['http://localhost:3000', 'https://bundle.love'];
     const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+    // if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    // }
     res.setHeader( 'Access-Control-Allow-Methods', '*')
     res.setHeader("Access-Control-Allow-Headers", "*");
     next();
