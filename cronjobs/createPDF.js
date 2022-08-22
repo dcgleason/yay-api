65c47e1e905772faee10ee627a5e9a447e4dc273
Dan Gleason <dannycgleason@gmail.com>
2022-08-22 18:10:10 -0400

    pdfmake update

--- cronjobs/createPDF.js
+++ cronjobs/createPDF.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 const Gift = require("../models/Gift")
 const PDFMake = require('pdfmake');
 
+let font_path_one = (__dirname+'/fonts/Roboto/Roboto-Regular.ttf');
+let font_path_two = (__dirname+'/fonts/Roboto/Roboto-Medium.ttf');
+let font_path_three = (__dirname+'/fonts/Roboto/Roboto-Italic.ttf');
+let font_path_four = (__dirname+'/fonts/Roboto/Roboto-MediumItalic.ttf');
+
 var fonts = {
 	Roboto: {
-		normal: '../fonts/Roboto/Roboto-Regular.ttf',
-		bold: '../fonts/Roboto/Roboto-Medium.ttf',
-		italics: '../fonts/Roboto/Roboto-Italic.ttf',
-		bolditalics: '../fonts/Roboto/Roboto-MediumItalic.ttf'
+		normal: font_path_one,
+		bold: font_path_two,
+		italics: font_path_three,
+		bolditalics: font_path_four
 	}
 };
 
@@ -17,11 +22,11 @@ var printer = new PDFMake(fonts);
 var docDefinition = {
 	content: [
 		'First paragraph',
-		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines, test test test'
 	]
 };
 
 var pdfDoc = printer.createPdfKitDocument(docDefinition);
-pdfDoc.pipe(fs.createWriteStream('pdfs/basics.pdf'));
+pdfDoc.pipe(fs.createWriteStream('pdfs/basic_two.pdf'));
 pdfDoc.end();
 
