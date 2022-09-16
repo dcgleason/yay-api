70e5749c3069a37c05a4b9156ebb70f970cf7cf2
Manny Santana <49816212+Manny-Santana@users.noreply.github.com>
2022-09-16 09:28:01 -0400

    updated comments in contribution schema

--- models/Contribution.js
+++ models/Contribution.js
@@ -7,17 +7,15 @@ const Schema = mongoose.Schema;
  * Contributers are added to a gift by a user
  * each contributer is associated with one gift
  * and are identified by their _id
- * each contributer has an array of questionIDs that contain the contents of
- * each question along with images and responses
- *
+ * each contribution will contain all the information necessary to make an entry. images message etc...
  */
 
 const contributionSchema = new Schema(
   {
-    name: String, //guest user name
+    name: String, //guest user name will be used to populate the entry on the page
     message: String, // message that will be attached to the entry in the book
-    associatedGiftID: String,
-    contributed: Boolean,
+    associatedGiftID: String, // which gift is this entry for
+    contributed: Boolean, //did the user submit a response? true or false
     ImageAddress: String, // the plan currently is to host these images in an s3 bucket and pull them down as needed when creating the pdf.
     email: String,
   },
