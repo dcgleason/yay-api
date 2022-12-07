98c25691fd24abe9f0060454b6586d9c4ebd080a
Dan Gleason <dannycgleason@gmail.com>
2022-12-07 09:28:15 -0500

    update to login code

--- routes/login.js
+++ routes/login.js
@@ -10,6 +10,7 @@ var LocalStrategy = require("passport-local").Strategy;
 var server = require('../server');
 
 // Package documentation - https://www.npmjs.com/package/connect-mongo
+
 const MongoStore = require("connect-mongo")(session);
 
 /**
@@ -118,8 +119,7 @@ const sessionStore = new MongoStore({
  */
 app.use(
   session({
-    //secret: process.env.SECRET,
-    secret: "some secret",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     store: sessionStore,
@@ -248,4 +248,44 @@ app.get("/login-failure", (req, res, next) => {
 
 
 
+/**
+ * -------------- HELPER FUNCTIONS ----------------
+ */
 
+/**
+ *
+ * @param {*} password - The plain text password
+ * @param {*} hash - The hash stored in the database
+ * @param {*} salt - The salt stored in the database
+ *
+ * This function uses the crypto library to decrypt the hash using the salt and then compares
+ * the decrypted hash/salt with the password that the user provided at login
+ */
+ function validPassword(password, hash, salt) {
+  var hashVerify = crypto
+    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
+    .toString("hex");
+  return hash === hashVerify;
+}
+
+/**
+ *
+ * @param {*} password - The password string that the user inputs to the password field in the register form
+ *
+ * This function takes a plain text password and creates a salt and hash out of it.  Instead of storing the plaintext
+ * password in the database, the salt and hash are stored for security
+ *
+ * ALTERNATIVE: It would also be acceptable to just use a hashing algorithm to make a hash of the plain text password.
+ * You would then store the hashed password in the database and then re-hash it to verify later (similar to what we do here)
+ */
+function genPassword(password) {
+  var salt = crypto.randomBytes(32).toString("hex");
+  var genHash = crypto
+    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
+    .toString("hex");
+
+  return {
+    salt: salt,
+    hash: genHash,
+  };
+}
