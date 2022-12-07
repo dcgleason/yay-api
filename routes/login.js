81e4c2f9ae599660f5d6a6952c5b11896f30a4c1
Dan Gleason <dannycgleason@gmail.com>
2022-12-06 20:08:28 -0500

    updated code for passport and login - signup

--- routes/login.js
+++ routes/login.js
@@ -4,52 +4,63 @@ const router = express.Router();
 var passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require("../models/User");
-var crypto = require("crypto");
-
+const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
+var passport = require("passport");
+var crypto = require("crypto");
+var LocalStrategy = require("passport-local").Strategy;
 
 // Package documentation - https://www.npmjs.com/package/connect-mongo
 const MongoStore = require("connect-mongo")(session);
 
+/**
+ * -------------- GENERAL SETUP ----------------
+ */
+
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
 require("dotenv").config();
 
-// Middleware that allows Express to parse through both JSON and x-www-form-urlencoded request bodies
-// These are the same as `bodyParser` - you probably would see bodyParser put here in most apps
+// Create the Express application
+var app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+/**
+ * -------------- DATABASE ----------------
+ */
 
-const connection = mongoose.createConnection(process.env.DB_STRING);
+/**
+ * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
+ * string into the `.env` file
+ *
+ * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
+ */
 
-//Note that the `connection` used for the MongoStore is the same connection that we are using above
-
-const sessionStore = new MongoStore({
- mongooseConnection: connection,
- collection: "sessions",
+const conn = "mongodb://devuser:123@localhost:27017/general_dev";
+//process.env.DB_STRING
+const connection = mongoose.createConnection(conn, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-function validPassword(password, hash, salt) {
-  var hashVerify = crypto
-    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
-    .toString("hex");
-  return hash === hashVerify;
-}
+// Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
+const UserSchema = new mongoose.Schema({
+  username: String,
+  hash: String,
+  salt: String,
+});
 
-function genPassword(password) {
-  var salt = crypto.randomBytes(32).toString("hex");
-  var genHash = crypto
-    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
-    .toString("hex");
+const User = connection.model("User", UserSchema);
 
-  return {
-    salt: salt,
-    hash: genHash,
-  };
-}
-
-
+/**
+ * This function is called when the `passport.authenticate()` method is called.
+ *
+ * If a user is found an validated, a callback is called (`cb(null, user)`) with the user
+ * object.  The user object is then serialized with `passport.serializeUser()` and added to the
+ * `req.session.passport` object.
+ */
 passport.use(
   new LocalStrategy(function (username, password, cb) {
     User.findOne({ username: username })
@@ -73,10 +84,21 @@ passport.use(
   })
 );
 
+/**
+ * This function is used in conjunction with the `passport.authenticate()` method.  See comments in
+ * `passport.use()` above ^^ for explanation
+ */
 passport.serializeUser(function (user, cb) {
   cb(null, user.id);
 });
 
+/**
+ * This function is used in conjunction with the `app.use(passport.session())` middleware defined below.
+ * Scroll down and read the comments in the PASSPORT AUTHENTICATION section to learn how this works.
+ *
+ * In summary, this method is "set" on the passport object and is passed the user ID stored in the `req.session.passport`
+ * object later on.
+ */
 passport.deserializeUser(function (id, cb) {
   User.findById(id, function (err, user) {
     if (err) {
@@ -86,30 +108,170 @@ passport.deserializeUser(function (id, cb) {
   });
 });
 
+/**
+ * -------------- SESSION SETUP ----------------
+ */
+
+/**
+ * The MongoStore is used to store session data.  We will learn more about this in the post.
+ *
+ * Note that the `connection` used for the MongoStore is the same connection that we are using above
+ */
+const sessionStore = new MongoStore({
+  mongooseConnection: connection,
+  collection: "sessions",
+});
+
+/**
+ * See the documentation for all possible options - https://www.npmjs.com/package/express-session
+ *
+ * As a brief overview (we will add more later):
+ *
+ * secret: This is a random string that will be used to "authenticate" the session.  In a production environment,
+ * you would want to set this to a long, randomly generated string
+ *
+ * resave: when set to true, this will force the session to save even if nothing changed.  If you don't set this,
+ * the app will still run but you will get a warning in the terminal
+ *
+ * saveUninitialized: Similar to resave, when set true, this forces the session to be saved even if it is uninitialized
+ *
+ * store: Sets the MemoryStore to the MongoStore setup earlier in the code.  This makes it so every new session will be
+ * saved in a MongoDB database in a "sessions" table and used to lookup sessions
+ *
+ * cookie: The cookie object has several options, but the most important is the `maxAge` property.  If this is not set,
+ * the cookie will expire when you close the browser.  Note that different browsers behave slightly differently with this
+ * behavior (for example, closing Chrome doesn't always wipe out the cookie since Chrome can be configured to run in the
+ * background and "remember" your last browsing session)
+ */
+app.use(
+  session({
+    //secret: process.env.SECRET,
+    secret: "some secret",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 30,
+    },
+  })
+);
+
+/**
+ * -------------- PASSPORT AUTHENTICATION ----------------
+ */
+
+/**
+ * Notice that these middlewares are initialized after the `express-session` middleware.  This is because
+ * Passport relies on the `express-session` middleware and must have access to the `req.session` object.
+ *
+ * passport.initialize() - This creates middleware that runs before every HTTP request.  It works in two steps:
+ *      1. Checks to see if the current session has a `req.session.passport` object on it.  This object will be
+ *
+ *          { user: '<Mongo DB user ID>' }
+ *
+ *      2.  If it finds a session with a `req.session.passport` property, it grabs the User ID and saves it to an
+ *          internal Passport method for later.
+ *
+ * passport.session() - This calls the Passport Authenticator using the "Session Strategy".  Here are the basic
+ * steps that this method takes:
+ *      1.  Takes the MongoDB user ID obtained from the `passport.initialize()` method (run directly before) and passes
+ *          it to the `passport.deserializeUser()` function (defined above in this module).  The `passport.deserializeUser()`
+ *          function will look up the User by the given ID in the database and return it.
+ *      2.  If the `passport.deserializeUser()` returns a user object, this user object is assigned to the `req.user` property
+ *          and can be accessed within the route.  If no user is returned, nothing happens and `next()` is called.
+ */
 app.use(passport.initialize());
 app.use(passport.session());
 
+/**
+ * -------------- ROUTES ----------------
+ */
 
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
+app.get("/", (req, res, next) => {
+  res.send("<h1>Home</h1>");
+});
+
+// When you visit http://localhost:3000/login, you will see "Login Page"
+app.get("/login", (req, res, next) => {
+  const form = '<h1>Login Page</h1><form method="POST" action="/login">\
+    Enter Username:<br><input type="text" name="username">\
+    <br>Enter Password:<br><input type="password" name="password">\
+    <br><br><input type="submit" value="Submit"></form>';
+
+  res.send(form);
+});
+
+// Since we are using the passport.authenticate() method, we should be redirected no matter what
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login-failure",
+    successRedirect: "login-success",
+  }),
+  (err, req, res, next) => {
+    if (err) next(err);
+  }
+);
+
+// When you visit http://localhost:3000/register, you will see "Register Page"
+app.get("/register", (req, res, next) => {
+  const form = '<h1>Register Page</h1><form method="post" action="register">\
+                    Enter Username:<br><input type="text" name="username">\
+                    <br>Enter Password:<br><input type="password" name="password">\
+                    <br><br><input type="submit" value="Submit"></form>';
+
+  res.send(form);
+});
+
+app.post("/register", (req, res, next) => {
+  const saltHash = genPassword(req.body.password);
+
+  const salt = saltHash.salt;
+  const hash = saltHash.hash;
+
+  const newUser = new User({
+    username: req.body.username,
+    hash: hash,
+    salt: salt,
   });
 
-
-  app.post('/signup', function(req, res) { 
-   var username = req.body.username;
-  var password = req.body.password;
-  var user = new User({username: username, password: password})
-  user.save(function(err) {
-    if (err) {
-        console.log(err);
-        res.send(500);
-      } else {
-        res.send(200);
-      }
-    });
+  newUser.save().then((user) => {
+    console.log(user);
   });
+
+  res.redirect("/login");
+});
+
+/**
+ * Lookup how to authenticate users on routes with Local Strategy
+ * Google Search: "How to use Express Passport Local Strategy"
+ *
+ * Also, look up what behavior express session has without a max age set
+ */
+app.get("/protected-route", (req, res, next) => {
+  console.log(req.session);
+  if (req.isAuthenticated()) {
+    res.send("<h1>You are authenticated</h1>");
+  } else {
+    res.send("<h1>You are not authenticated</h1>");
+  }
+});
+
+// Visiting this route logs the user out
+app.get("/logout", (req, res, next) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+app.get("/login-success", (req, res, next) => {
+  console.log(req.session);
+  res.send("You successfully logged in.");
+});
+
+app.get("/login-failure", (req, res, next) => {
+  res.send("You entered the wrong password.");
+});
+
 
 
 
