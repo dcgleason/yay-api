cad4301cb2a72b345aaa13303cf9ae69700cce6e
Dan Gleason <dannycgleason@gmail.com>
2023-02-03 11:58:44 -0500

    update took out audio concat route

--- routes/contribution.js
+++ routes/contribution.js
@@ -4,6 +4,8 @@ const Contribution = require("../models/Contribution");
 // Import
 const Transloadit = require('transloadit')
 
+var convertapi = require('convertapi')('vlman1y8KNerUdy7');
+
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //GET ROUTES
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -51,12 +53,12 @@ router.post('/create-document', (req, res) => {
     },
     body: {
       template: {
-        id: 'REPLACE_TEMPLATE_ID', // fill in
+        id: 'REPLACE_TEMPLATE_ID', // fill in with req.body.tempalateID
         data: {id: 123, name: 'John Smith', birthdate: '2000-01-01', role: 'Developer'}   // fill in with req.body
       },
       format: 'pdf',
-      output: 'base64',
-      name: 'Invoice 123'
+      output: 'url',
+      name: 'Contributor Name + Date' // fill in with req.body.contributorName + Date.now()
     },
     json: true
   };
@@ -64,71 +66,29 @@ router.post('/create-document', (req, res) => {
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
   
-    console.log(body); // upload to MongoDB contributor model
+    console.log(body); // upload to MongoDB contributor model as base64
   });
 
 });
   
 
+router.post("/create-book", (req, res) => {
+  // get the contribution pages and create a book by first name alphabetical order then submits to Lulu for printing
 
-router.post('/audio-concat', (req, res) => {
-  
-// Init
-const transloadit = new Transloadit({
-  authKey: 'YOUR_TRANSLOADIT_KEY',
-  authSecret: 'MY_TRANSLOADIT_SECRET',
-})
 
-// Set Encoding Instructions
-const options = {
-  files: {
-    myfile_1: './dutch-anthem.mp3',
-  },
-  params: {
-    steps: {
-      ':original': {
-        robot: '/upload/handle',
-      },
-      imported_postroll: {
-        robot: '/http/import',
-        url: 'https://demos.transloadit.com/inputs/german-anthem.mp3',
-      },
-      concatenated: {
-        use: {
-          steps: [
-            {
-              name: ':original',
-              fields: 'file',
-              as: 'audio_1',
-            }, 
-,            {
-              name: 'imported_postroll',
-              as: 'audio_2',
-            }, 
-          ],
-        },
-        robot: '/audio/concat',
-        result: true,
-        ffmpeg_stack: 'v4.3.1',
-      },
-      exported: {
-        use: ['imported_postroll', 'concatenated', ':original'],
-        robot: '/s3/store',
-        credentials: 'YOUR_AWS_CREDENTIALS',
-        path: '${unique_prefix}/both-anthems.${file.ext}',
-        url_prefix: 'https://demos.transloadit.com/',
-      },
-    },
-  },
-}
+convertapi.convert('merge', {
+    Files: [
+      '/path/to/bundlePageSpread.pdf', // url to the bundle page spread
+      '/path/to/Bundle Gifter Console Wireframe.pdf'
+    ],
+    StoreFile: true,
+  }, 'pdf').then(function(result) {
+      console.log(result.file.url); // donloadable link! --> to send to Lulu
+});
 
-// Execute
-const result = await transloadit.createAssembly(options)
+});
 
-// Show results
-console.log({ result })
 
-})
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //  PUT / UPDATE ROUTES
--- testing/merge.js
+++ testing/merge.js
@@ -1,10 +1,11 @@
-var convertapi = require('convertapi')('0qXtopGkSdrs5HaH');
+var convertapi = require('convertapi')('vlman1y8KNerUdy7');
 
 convertapi.convert('merge', {
     Files: [
         'https://us1.pdfgeneratorapi.com/api/v4/documents/20772/6f3e2310eaa7c364c93bc273257837bd/share',
         'https://us1.pdfgeneratorapi.com/api/v4/documents/20772/fc9ae15eac003cdb5f6f8a0af0187e9e/share',
-    ]
+    ],
+    StoreFile: true,
 }, 'pdf').then(function(result) {
-    result.saveFiles('pdfs');
+    console.log(result.file.url);
 });
