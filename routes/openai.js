a8cec0777620286d8aa11f23b21c00ee190fe6b9
Dan Gleason <dannycgleason@gmail.com>
2023-04-01 13:53:36 -0400

    updae to heroku

--- routes/openai.js
+++ routes/openai.js
@@ -2,7 +2,9 @@ const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 const router = express.Router();
 
-
+router.get("/", (req, res) => {
+    res.send("OpenAI api home page");
+});
 
 router.post("/gift", async (req, res) => {
     
