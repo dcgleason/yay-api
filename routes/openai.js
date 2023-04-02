ce11034c76008a705218dc87ffd6548ea8030c96
Dan Gleason <dannycgleason@gmail.com>
2023-04-02 09:30:19 -0400

    updated temperature of ai model

--- routes/openai.js
+++ routes/openai.js
@@ -24,8 +24,8 @@ const openai = new OpenAIApi(configuration);
 const response = await openai.createCompletion({
   model: "text-davinci-003",
   prompt: `Generate one thoughtful idea for me to give ${recipient}, we are related via as ${relation}. ${recipient} is ${age}-year-old ${gender} and is interested in ${interests}. The occasion is for ${occasion} and my budget is ${budget}. Make the gift appropriate for ${recipient}'s age, gender, and pick only 1 of ${recipient}'s interests, unless you can nicely incorperate mnultiple interests into one gift idea. Make the gift as creative and as personal as possible. When presenting the gift idea, include the gift idea as "Give X" where X is the idea, then follow that with a brief description of X (your idea). Generate a new and different idea with each submission. Additional information: ${additionalInfo}`,
-  temperature: 0.7,
-  max_tokens: 250,
+  temperature: 0.5,
+  max_tokens: 200,
 });
 
 console.log("response" + response.data.choices[0].text);
