4cef14d145f117a3d496993f7309aa1bc033a8b2
Dan Gleason <dannycgleason@gmail.com>
2023-06-22 16:06:59 -0400

    update

--- models/User.js
+++ models/User.js
@@ -1,10 +1,7 @@
-
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-/**
- * each user can initiate a Gift <-- user can be placed inside of the gift schema as owner of the gift
- */
+
 const userSchema = new Schema(
   {
     bookID: { type: Schema.Types.ObjectId, ref: 'Book' },
@@ -14,17 +11,13 @@ const userSchema = new Schema(
     name: String,
     shippingAddress: String,
     giftOwnerEmail: String,
-    refreshToken: String, // Add this line
+    refreshToken: String,
     lastEmailed: Date,
-      // have this be an array of 5 prompts, each are a String
-      prompts: {
-        type: Map,
-        of: String
+    prompts: [String],
   },
   { timestamps: true }
-});
+);
 
 const User = mongoose.model("User", userSchema);
-
 
 module.exports = User;
