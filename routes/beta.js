3465556034027e28a84b8f7f275c3634dd860c1b
Dan Gleason <dannycgleason@gmail.com>
2023-06-22 08:50:41 -0400

    update to watilist route

--- routes/beta.js
+++ routes/beta.js
@@ -1,30 +1,24 @@
 const express = require('express')
 const router = express.Router()
-const Tester = require("../models/BetaTesters")
+const Waitlister = require("../models/Waitlister")
 
 
 //beta sign up route - /beta/signup
-router.post('/signup',async (req, res)=>{
-    const tester = {
-        name:req.body.name,
-        email:req.body.email, 
-        referralSource: req.body.referralSource
+router.post('/waitlist', async (req, res) => {
+    try {
+        const {email} = req.body
+        const waitlister = await Waitlister.create({email})
+        res.json(waitlister)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).json({
+            userFound: false,
+            error: true,
+            message: "Server error"
+        })
     }
-
-    
-
-    await Tester.create(tester, (err, createdItem)=>{
-        if(err){
-            console.log(err)
-            res.sendStatus(500)
-        }
-        else {
-            console.log(createdItem)
-            res.sendStatus(200)
-
-        }
-    })
-})
+  
+}
 
 
 
