a79d41b3d1be02c2eeaff709667e06033f08cbdb
Dan Gleason <dannycgleason@gmail.com>
2023-07-25 07:37:43 -0400

    uipdate to cosole logs

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,22 +2,21 @@ const express = require('express')
 const router = express.Router()
 const stripe = require('stripe')(process.env.STRIPE_SECRET) // secret key for test environment, to be replaced by actual prod secret key when we start taking orderes
 
-
-
-// Change the route from a GET to a POST
 router.post('/secret', async (req, res) => {
   console.log('Making requests!');
 
-  // Extract the email from the request body
+  // Extract the email and amount from the request body
   const customerEmail = req.body.email;
+  const amount = req.body.amount;
 
   const intent = await stripe.paymentIntents.create({
     currency: 'usd',
-    amount: 7900,
+    amount: amount,
     metadata: { integration_check: 'accept_a_payment' },
     receipt_email: customerEmail, // Add the receipt_email property
   });
 
+  console.log("request complete + intent: ", intent)
   res.json(intent);
 });
  
