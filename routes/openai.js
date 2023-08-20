7699c35d66bde722359884d7239a6d3f3ca6a232
dcgleason <danny.c.gleason@gmail.com>
2023-08-20 16:06:05 -0400

    update

--- routes/openai.js
+++ routes/openai.js
@@ -23,7 +23,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 const response = await openai.createChatCompletion({
   model: "gpt-4",
-  messages: [{ role: "user", content: `Create a personalized proposal plan that tells my unique love story (${meetStory}), caters to my relationship and relationship milestones (${milestone}), memories (${memories}), my location (${location}),  my budget ${budget}, and timeline (${timeline}), my partners interets (${interests}) and includes specific venues, vendors, and tips for managing the proposal. Mention why Bundl is a good idea (to include my parnter's family) and provide resources and value as much as possible. Note any additional info (${additionalInfo})`}],
+  messages: [{ role: "user", content: `Create a personalized proposal plan that tells my unique love story (${meetStory}), caters to my relationship and relationship milestones (${milestone}), memories (${memories}), my location (${location}),  my budget ${budget}, timeline (${timeline}), my partners interets (${interests}) and includes specific venues, vendors, and tips for managing the proposal. Mention the importance of including your partner's family in the proposal. Note any additional info (${additionalInfo})`}],
   max_tokens: 900,
   n: 1,
   temperature: 0.5,
