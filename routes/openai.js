d3827847c70670771bea57c16871583dc186044b
dcgleason <danny.c.gleason@gmail.com>
2023-08-22 08:39:27 -0400

    update

--- routes/openai.js
+++ routes/openai.js
@@ -23,10 +23,10 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 const response = await openai.createChatCompletion({
   model: "gpt-4",
-  messages: [{ role: "user", content: `Create a marriage proposal idea that has elements of my unique love story (${meetStory}), alludes ro or somehow includes a reference to my relationship and relationship milestones (${milestone}), memories (${memories}), is in my desired location (${location}), takes into consideration my budget ${budget}, timeline (${timeline}), and my partners interets (${interests}) and includes specific links to top reviewed venues, vendors. Add a few tips for managing the proposal...(additionally, mention the importance of including your partner's family in the proposal). Note any additional info (${additionalInfo})`}],
+  messages: [{ role: "user", content: `Create a marriage proposal idea that has elements of my unique love story (${meetStory}), alludes to my relationship and relationship milestones (${milestone}), memories (${memories}), is in my desired location (${location}), takes into consideration my budget ${budget}, timeline (${timeline}), and ideally my partners interets (${interests}) and includes specific links to top reviewed venues, vendors. Add a few tips for managing the proposal...(additionally, suggest including my partner's family in the proposal if feasible). Note any additional info (${additionalInfo})`}],
   max_tokens: 300,
   n: 1,
-  temperature: 0.5,
+  temperature: 0.8,
 });
 
 console.log("response" + response.data.choices[0].message.content.trim());
