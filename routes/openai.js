ab42a9dfefeaf9c786587a148d13fe10ad04a136
dcgleason <danny.c.gleason@gmail.com>
2023-08-22 13:07:34 -0400

    update token legth

--- routes/openai.js
+++ routes/openai.js
@@ -24,7 +24,7 @@ const openai = new OpenAIApi(configuration);
 const response = await openai.createChatCompletion({
   model: "gpt-4",
   messages: [{ role: "user", content: `Create a marriage proposal idea that has elements of my unique love story (${meetStory}), alludes to my relationship and relationship milestones (${milestone}), memories (${memories}), is in my desired location (${location}), takes into consideration my budget ${budget}, timeline (${timeline}), and ideally my partners interets (${interests}) and includes the names of top reviewed venues, vendors we should go to in your recommendation / idea. Add a few tips for managing the proposal...(additionally, suggest including my partner's family in the proposal, using Bundl book -- like to www.mobile.givebundl.com -- to do so). Note any additional info (${additionalInfo})`}],
-  max_tokens: 300,
+  max_tokens: 500,
   n: 1,
   temperature: 0.8,
 });
